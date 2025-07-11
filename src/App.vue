a7389d147f3a80214258b4c9023aba299e6bc0b3
Rizkyalii <ikymlnaali09@gmail.com>
2025-07-11 06:15:01 -0700

    perubahan title

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
 
   <Navbar />
-  <!-- <Login /> -->
-   <!-- <router-view /> -->
 </template>
+
 <script setup>
 import Navbar from './components/Main.vue'
-// import { RouterView } from 'vue-router'
 </script>
+
 <!-- npx json-server --watch db.json --port 3000 -->
 
 <style scoped>
